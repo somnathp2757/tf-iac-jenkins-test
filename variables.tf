41994e62e952c13f888886e488d11d232269d794
adityaghuraiya <55561859+adityaghuraiya@users.noreply.github.com>
2023-02-22 10:40:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable project_id {
+  type        = string
+  default     = "cisco-sdwan-eveng "
+  description = "description"
+}
+
+variable region {
+  type        = string
+  default     = "us-central1"
+  description = "description"
+}
+
+variable zone {
+  type        = string
+  default     = "us-central1-a"
+  description = "description"
+}
+
+variable vpc_name {
+  type        = string
+  default     = "adi-test"
+  description = "description"
+}
