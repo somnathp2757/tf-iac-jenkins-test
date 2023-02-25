39420af3dc3ee60047790f65411c86ee2f0b5368
adityaghuraiya <55561859+adityaghuraiya@users.noreply.github.com>
2023-02-25 19:46:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,34 @@
 variable project_id {
   type        = string
-  default     = "cisco-sdwan-eveng"
+  default     = "cisco-sdwan-eveng "
   description = "description"
 }
 
 variable region {
   type        = string
   default     = "us-central1"
-  description = "description"
+  description = "region"
 }
 
 variable zone {
   type        = string
-  default     = "us-central1-a"
-  description = "description"
+  default     = "zone name"
+  description = "b"
 }
 
-variable vpc_name {
-  type        = string
-  default     = "adi-test"
-  description = "description"
+variable qty {
+  type        = number
+  description = "number of vpc"
 }
+
+variable email_id {
+  type        = string
+  description = "number of vpc"
+}
+
+variable machine_type {
+  type        = string
+  description = "number of vpc"
+  default     = "e2-medium"
+}
+
