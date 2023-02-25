d97329c16e132258057662b39aea59b07a40d678
adityaghuraiya <55561859+adityaghuraiya@users.noreply.github.com>
2023-02-25 19:45:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,52 @@ provider "google" {
 
   project = var.project_id
   region  = var.region
-  zone    = var.zone
+  zone    = "${var.region}-${var.zone}"
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = var.vpc_name
+  count = var.qty
+  name = "${var.email_id}-${count.index + 1}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet1" {
+  count         = var.qty
+  name          = "${var.email_id}-${count.index + 1}-subnet1"
+  ip_cidr_range = "10.${count.index + 1}.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc_network[count.index]
+}
+
+resource "google_compute_instance" "default" {
+  count        = var.qty
+  name         = "${google_compute_subnetwork.subnet1[count.index].name}-${vm}-${count.index}"
+  machine_type = var.machine_type
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network[count.index].name
+    subnetwork = google_compute_subnetwork.subnet1[count.index].name
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
 }
